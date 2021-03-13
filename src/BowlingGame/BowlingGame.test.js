16178993592658e4eb6488c8660de4d109cf6dc7
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-13 16:30:47 +0100

    add new spare test with different second throw

--- src/BowlingGame/BowlingGame.test.js
+++ src/BowlingGame/BowlingGame.test.js
@@ -14,11 +14,18 @@ test("Threes: all throws are threes", () => {
   expect(game.calculateScore()).toBe(60);
 });
 
-test("First Throw Spare, second OpenFrame, rest zeros", () => {
+test("First Spare, second OpenFrame (3, 5), rest zeros", () => {
   game.spare(4, 6);
   game.openFrame(3, 5);
   manyOpenFrames(8, 0, 0);
   expect(game.calculateScore()).toBe(21);
+});
+
+test("First Spare, second OpenFrame (7, 1), rest zeros", () => {
+  game.spare(4, 6);
+  game.openFrame(7, 1);
+  manyOpenFrames(8, 0, 0);
+  expect(game.calculateScore()).toBe(25);
 });
 
 const manyOpenFrames = (count = 10, firstThrow = 0, secondThrow = 0) => {
