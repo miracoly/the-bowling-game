0fd6badd6c92af9054ee9d4a33390ca20c2e6948
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-13 15:34:51 +0100

    adding test 'first spare, second open, rest zeros'

--- src/BowlingGame/BowlingGame.test.js
+++ src/BowlingGame/BowlingGame.test.js
@@ -14,6 +14,13 @@ test("Threes: all throws are threes", () => {
   expect(game.getScore()).toBe(60);
 });
 
+test("First Throw Spare, second OpenFrame, rest zeros", () => {
+  game.spare(4, 6);
+  game.openFrame(3, 5);
+  manyOpenFrames(8, 0, 0);
+  expect(game.getScore()).toBe(21);
+});
+
 const manyOpenFrames = (count = 10, firstThrow = 0, secondThrow = 0) => {
   for (let frameNumber = 0; frameNumber < count; frameNumber++) {
     game.openFrame(firstThrow, secondThrow);
