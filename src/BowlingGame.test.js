da5e360ca21a0228c70c33343fbd5072d2a9d025
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-13 12:54:23 +0100

    adding new failing test 'all throws are threes'

--- src/BowlingGame.test.js
+++ src/BowlingGame.test.js
@@ -2,5 +2,16 @@ const BowlingGame = require("./BowlingGame");
 
 test("GutterBalls: all throws are zeros", () => {
   const game = new BowlingGame();
+  for (let frameNumber = 0; frameNumber < 10; frameNumber++) {
+    game.openFrame(0, 0);
+  }
   expect(game.getScore()).toBe(0);
+});
+
+test("Threes: all throws are threes", () => {
+  const game = new BowlingGame();
+  for (let frameNumber = 0; frameNumber < 10; frameNumber++) {
+    game.openFrame(3, 3);
+  }
+  expect(game.getScore()).toBe(60);
 });
