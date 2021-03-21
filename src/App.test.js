af77297ecde0d29d6ecc9e32f5e7f3818d8ec15d
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-21 17:52:41 +0100

    add failing test for bowlingGame to contain 10 frames

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,17 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-beforeEach(() => render(<App />));
-
 test("Renders BowlingGame component", () => {
-  expect(screen.getByTestId("bowling-game")).toBeInTheDocument();
+  render(<App />);
+  const bowlingGame = screen.getByTestId("bowling-game");
+  expect(bowlingGame).toBeInTheDocument();
+});
+
+test("BowlingGame contains 10 Frames", () => {
+  render(<App />);
+  const bowlingGame = screen.getByTestId("bowling-game");
+  for (let i = 1; i <= 10; i++) {
+    const frame = screen.getByTestId(`frame-${i}`);
+    expect(bowlingGame).toContainElement(frame);
+  }
 });
