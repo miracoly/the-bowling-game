b5ef810e5105e457d53483249068dfa7be373735
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-21 15:36:05 +0100

    add test for rendering BowlingGame component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("Renders BowlingGame component", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+
+  expect(root.querySelector("bowling-game").textContent).toBe("Bowling Game");
 });
---
+++ src/components/BowlingGame/BowlingGame.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BowlingGame = () => {
+  return <div id="bowling-game"></div>;
+};
+
+export default BowlingGame;
