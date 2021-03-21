fd5ca0f2ed494eaaebaac5037f3c2b1fbbe98ca6
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-21 22:07:17 +0100

    adding styles

--- src/components/BowlingGame/BowlingGame.js
+++ src/components/BowlingGame/BowlingGame.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Frame from "../Frame/Frame";
+import "./BowlingGame.css";
 
 const BowlingGame = () => {
   const frames = [];
 
   for (let i = 1; i <= 10; i++) {
-    const key = `frame-${i}`;
-    frames.push(<Frame key={key} id={key} />);
+    const props = {
+      key: `frame-${i}`,
+      index: i,
+      isLastFrame: i === 10,
+    };
+
+    frames.push(<Frame {...props} />);
   }
 
   return (
-    <div id="bowling-game" data-testid="bowling-game">
+    <div className="bowling-game" data-testid="bowling-game">
       {frames}
     </div>
   );
--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Frame.css";
 
-const Frame = ({ id }) => {
-  return <div data-testid={id}>{`Frame ${id}`}</div>;
+const Frame = ({ index, isLastFrame }) => {
+  return (
+    <div
+      data-testid={`frame-${index}`}
+      className={`frame ${isLastFrame ? "last-frame" : null}`}
+    >
+      <div className="frame-index">{index}</div>
+      <div className="throws">
+        <div className="first-throw">3</div>
+        <div className="second-throw">4</div>
+        {isLastFrame ? <div className="third-throw">4</div> : null}
+      </div>
+      <div className="frame-total">7</div>
+    </div>
+  );
 };
 
 export default Frame;
