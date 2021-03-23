c7a9078a4ebcd13e54610223744e208355305236
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-23 14:15:57 +0100

    refactor BowlingGameContainer.test.js to reduce repetition

--- src/components/BowlingGameContainer/BowlingGameContainer.test.js
+++ src/components/BowlingGameContainer/BowlingGameContainer.test.js
@@ -4,64 +4,71 @@ import BowlingGameContainer from "./BowlingGameContainer";
 import BowlingGame from "../../BowlingGame/BowlingGame";
 import { manyOpenFrames } from "../../BowlingGame/BowlingGame.test";
 
+let game;
+
+beforeEach(() => {
+  game = new BowlingGame();
+});
+
 test("UI: GutterBalls: all throws are zeros", () => {
-  const game = new BowlingGame();
+  /* Set game to test case */
   manyOpenFrames(game, 10, 0, 0);
   render(<BowlingGameContainer initialBowlingGame={game} />);
 
   /* Test total score to equal 0 */
-  const score = screen.getByTestId("score");
-  expect(score).toHaveTextContent("0");
+  expectTotalScoreToBe(0);
 
   /* Test first and second throw to equal 0 */
   for (let i = 1; i <= 10; i++) {
-    const firstThrow = screen.getByTestId(`first-throw-${i}`);
-    const secondThrow = screen.getByTestId(`second-throw-${i}`);
-    const frameTotal = screen.getByTestId(`frame-total-${i}`);
-    expect(firstThrow).toHaveTextContent("0");
-    expect(secondThrow).toHaveTextContent("0");
-    expect(frameTotal).toHaveTextContent("0");
+    expectFrameToBe(i, 0, 0, 0);
   }
 });
 
 test("UI: Threes: all throws are threes", () => {
-  const game = new BowlingGame();
+  /* Set game to test case */
   manyOpenFrames(game, 10, 3, 3);
   render(<BowlingGameContainer initialBowlingGame={game} />);
 
-  /* Test total score to equal 60 */
-  const score = screen.getByTestId("score");
-  expect(score).toHaveTextContent("60");
+  /* Test UI Score */
+  expectTotalScoreToBe(60);
 
   /* Test first and second throw to equal 3 and frameTotal 6 */
   for (let i = 1; i <= 10; i++) {
-    const firstThrow = screen.getByTestId(`first-throw-${i}`);
-    const secondThrow = screen.getByTestId(`second-throw-${i}`);
-    const frameTotal = screen.getByTestId(`frame-total-${i}`);
-    expect(firstThrow).toHaveTextContent("3");
-    expect(secondThrow).toHaveTextContent("3");
-    expect(frameTotal).toHaveTextContent("6");
+    expectFrameToBe(i, 3, 3, 6);
   }
 });
 
 test("UI: First Spare, second OpenFrame (3, 5), rest zeros", () => {
-  const game = new BowlingGame();
+  /* Set game to test case */
   game.spare(4, 6);
   game.openFrame(3, 5);
   manyOpenFrames(game, 8, 0, 0);
-
   render(<BowlingGameContainer initialBowlingGame={game} />);
 
-  /* Test total score to equal 60 */
-  const score = screen.getByTestId("score");
-  expect(score).toHaveTextContent("21");
+  /* Test UI Score */
+  expectTotalScoreToBe(21);
 
   /* Test UI of first two frames */
-  const { getByTestId } = screen;
-  expect(getByTestId(`first-throw-1`)).toHaveTextContent("4");
-  expect(getByTestId(`second-throw-1`)).toHaveTextContent("6");
-  expect(getByTestId(`frame-total-1`)).toHaveTextContent("13");
-  expect(getByTestId(`first-throw-2`)).toHaveTextContent("3");
-  expect(getByTestId(`second-throw-2`)).toHaveTextContent("5");
-  expect(getByTestId(`frame-total-2`)).toHaveTextContent("8");
+  expectFrameToBe(1, 4, 6, 13);
+  expectFrameToBe(2, 3, 5, 8);
 });
+
+/* Helper Functions to test UI */
+
+const expectTotalScoreToBe = (score) => {
+  const uiScore = screen.getByTestId("score");
+  expect(uiScore).toHaveTextContent(score.toString());
+};
+
+const expectFrameToBe = (frameIndex, firstThrow, secondThrow, frameTotal) => {
+  const { getByTestId } = screen;
+  expect(getByTestId(`first-throw-${frameIndex}`)).toHaveTextContent(
+    firstThrow.toString()
+  );
+  expect(getByTestId(`second-throw-${frameIndex}`)).toHaveTextContent(
+    secondThrow.toString()
+  );
+  expect(getByTestId(`frame-total-${frameIndex}`)).toHaveTextContent(
+    frameTotal.toString()
+  );
+};
