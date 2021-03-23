66e2b2e320954a3d6f35d3da84b64e37892cbd2c
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-23 13:18:22 +0100

    add new ui test all threes

--- src/components/BowlingGameContainer/BowlingGameContainer.test.js
+++ src/components/BowlingGameContainer/BowlingGameContainer.test.js
@@ -6,7 +6,7 @@ import { manyOpenFrames } from "../../BowlingGame/BowlingGame.test";
 
 test("GutterBalls: all throws are zeros", () => {
   const game = new BowlingGame();
-  manyOpenFrames(10, 0, 0);
+  manyOpenFrames(game, 10, 0, 0);
   render(<BowlingGameContainer initialBowlingGame={game} />);
 
   /* Test total score to equal 0 */
@@ -17,7 +17,30 @@ test("GutterBalls: all throws are zeros", () => {
   for (let i = 1; i <= 10; i++) {
     const firstThrow = screen.getByTestId(`first-throw-${i}`);
     const secondThrow = screen.getByTestId(`second-throw-${i}`);
+    const frameTotal = screen.getByTestId(`frame-total-${i}`);
     expect(firstThrow).toHaveTextContent("0");
     expect(secondThrow).toHaveTextContent("0");
+    expect(frameTotal).toHaveTextContent("0");
+  }
+});
+
+test("Threes: all throws are threes", () => {
+  const game = new BowlingGame();
+  manyOpenFrames(game, 10, 3, 3);
+  render(<BowlingGameContainer initialBowlingGame={game} />);
+  console.log("threes", game);
+
+  /* Test total score to equal 60 */
+  const score = screen.getByTestId("score");
+  expect(score).toHaveTextContent("60");
+
+  /* Test first and second throw to equal 3 and frameTotal 6 */
+  for (let i = 1; i <= 10; i++) {
+    const firstThrow = screen.getByTestId(`first-throw-${i}`);
+    const secondThrow = screen.getByTestId(`second-throw-${i}`);
+    const frameTotal = screen.getByTestId(`frame-total-${i}`);
+    expect(firstThrow).toHaveTextContent("3");
+    expect(secondThrow).toHaveTextContent("3");
+    expect(frameTotal).toHaveTextContent("6");
   }
 });
